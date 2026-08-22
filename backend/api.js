2663b09a8b23f020ed6832eb026acf8ecb75affd
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-3.local>
2026-08-22 19:21:05 +0800

    Prepare backend for Vercel deployment

---
+++ backend/api.js
@@ -0,0 +1,27 @@
+import 'dotenv/config.js'
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+connectDB()
+
+app.use("/api/food", foodRouter)
+app.use("/images", express.static("uploads"))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+})
+
+export default app
